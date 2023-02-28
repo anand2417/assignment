4d04d6cd6b2e62789213ca48c063a0c55e4c1511
anand2417 <126228050+anand2417@users.noreply.github.com>
2023-02-28 16:44:28 +0530

    Delete assignment directory

--- assignment/springboot-std-project-main/src/main/java/com/animal/repository/AnimalRepository.java
+++
@@ -1,9 +0,0 @@
-package com.animal.repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.animal.bean.Animal;
-
-public interface AnimalRepository extends MongoRepository<Animal,Integer> {
-
-}
